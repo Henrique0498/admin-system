bfe7ac3a5582e305b6804557b9b9071558d224c0
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-05-27 09:14:54 -0300

    Update  - Siderbar
    Adicionei a foto de usuario na sideBar.

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -11,6 +11,29 @@ const SideBar = () => {
         <Image src="/img/icon-192.png" width={40} height={40} />
         {menu && <h2>Adm System</h2>}
       </div>
+
+      <div className="user transition-all">
+        <div className="userPhoto transition-all">
+          <div className="photo transition-all">
+            <Image
+              src="/img/user/profile.jpg"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+        {menu && <h2>Amanda</h2>}
+      </div>
+
+      <ul>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+        <li>A</li>
+      </ul>
     </S.Container>
   )
 }
--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -23,5 +23,32 @@ export const Container = styled('nav')<ContainerProps>`
         font-size: 16px;
       }
     }
+
+    .user {
+      display: flex;
+      align-items: center;
+      width: ${menu ? '250' : '60'}px;
+      flex-direction: ${menu ? 'column' : 'row'};
+      min-height: 60px;
+
+      .userPhoto {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        padding: ${menu ? '10' : '0'}px 0;
+      }
+
+      .photo {
+        width: ${menu ? '80' : '44'}px;
+        height: ${menu ? '80' : '44'}px;
+        position: relative;
+        overflow: hidden;
+        border-radius: 50%;
+      }
+    }
   `}
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
