980d556c348469b03dbd4e17fe9fd9448b8d33d6
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-06-02 12:35:10 -0300

    Add - ChartJS

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,7 +1,76 @@
+import { Line } from 'react-chartjs-2'
 import * as S from './styles'
 
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  BarElement
+} from 'chart.js'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+  BarElement
+)
+
 const HomeTemplate = () => {
-  return <S.Container>home</S.Container>
+  const data = {
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(153, 102, 255, 0.5)',
+            'rgba(255, 159, 64, 0.5)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      layout: {
+        padding: 0
+      },
+      plugins: {
+        legend: {}
+      }
+    }
+  }
+
+  return (
+    <S.Container>
+      <S.CardLarge className="w-ful bg-gray-50 dark:bg-gray-800 rounded-md shadow-sm hover:shadow-md relative">
+        <Line {...data} />
+      </S.CardLarge>
+    </S.Container>
+  )
 }
 
 export default HomeTemplate
--- src/components/pages/Home/styles.ts
+++ src/components/pages/Home/styles.ts
@@ -2,4 +2,14 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   height: 100%;
+  padding: 16px;
+`
+
+export const CardLarge = styled.div`
+  overflow: hidden;
+  transition: all ${({ theme }) => theme.animation.duration.slow};
+
+  :hover {
+    transform: scale(1.005);
+  }
 `
