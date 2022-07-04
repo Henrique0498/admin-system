14014cce15d87088cb7d233772a5b71aec26c406
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-07-04 17:16:49 -0300

    Add - Testing

---
+++ src/components/atoms/Button/Outline/ButtonOutline.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+
+import theme from '../../../../styles/theme'
+
+import ButtonOutline from './index'
+
+describe('<ButtonOutline/>', () => {
+  it('should render the heading', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <ButtonOutline>Button</ButtonOutline>
+      </ThemeProvider>
+    )
+
+    expect(screen.getByRole('button', { name: /button/i })).toBeInTheDocument()
+  })
+  it('should render correctly', () => {
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <ButtonOutline>Button</ButtonOutline>
+      </ThemeProvider>
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
