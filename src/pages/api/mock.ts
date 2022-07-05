0bc47a2e4fd492ee7dfb36fc7d14db0326562e90
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-07-05 17:44:13 -0300

    Add - nestJS

---
+++ src/pages/api/mock.ts
@@ -0,0 +1,78 @@
+export const user = {
+  id: '01',
+  username: 'henrique',
+  name: 'henrique lopes',
+  lastAccess: '6565',
+  email: 'ta',
+  balance: '35235',
+  expensesTotal: '6565',
+  spendingTotal: '6565',
+  expenses: [
+    {
+      name: 'tal nome',
+      value: '1000',
+      valueType: '02',
+      id: '154',
+      card: '0551',
+      date: '02',
+      duaDate: '03',
+      installments: '2/16',
+      paymentMethod: ''
+    },
+    {
+      name: 'tal nome',
+      value: '1000',
+      valueType: '02',
+      id: '1154',
+      date: '02',
+      duaDate: '03',
+      paymentMethod: ''
+    }
+  ],
+  spending: [
+    {
+      name: 'tal nome',
+      value: '1000',
+      id: '1354',
+      card: '0551',
+      date: '02',
+      duaDate: '03',
+      paymentMethod: ''
+    },
+    {
+      name: 'tal nome',
+      value: '1000',
+      id: '1544',
+      date: '02',
+      duaDate: '03',
+      paymentMethod: ''
+    }
+  ],
+  cards: [
+    {
+      name: 'card tal',
+      id: '2342',
+      due: '03',
+      shutdown: '2541',
+      value: '56444',
+      expenses: [],
+      spending: []
+    }
+  ],
+  earnings: [
+    {
+      id: 'd343',
+      name: 'tal',
+      value: '2454',
+      date: '02'
+    }
+  ],
+  income: [
+    {
+      id: 'd343',
+      name: 'tal',
+      value: '2454',
+      date: '02'
+    }
+  ]
+}
