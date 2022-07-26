d5d54816a532233d5344cea283c109691add0fd1
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-07-26 16:15:43 -0300

    Change - Responsive

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -51,7 +51,7 @@ const Login = () => {
     }
 
     return (
-      <Grid item md={12} lg={8}>
+      <Grid item md={12} lg={10}>
         <S.LoginDesktopCard>
           <S.SingContainer className="shadow-md" path={path}>
             <div className={path === 'singUp' ? 'active' : ''}>
@@ -90,37 +90,6 @@ const Login = () => {
       </Grid>
     </S.Container>
   )
-
-  // return (
-  //   <S.Container>
-  //     <S.RecoverPassword className="shadow-md"></S.RecoverPassword>
-  //     <S.SingContainer className="shadow-md" path={path}>
-  //       <div className={path === 'singUp' ? 'active' : ''}>
-  //         <S.SingInCard>
-  //           <h2>Já tem uma conta?</h2>
-  //           <p>Bem vindo de volta!</p>
-
-  //           <ButtonOutline onClick={() => setPath('singIn')}>
-  //             Entrar
-  //           </ButtonOutline>
-  //         </S.SingInCard>
-  //         <SingUp />
-  //       </div>
-
-  //       <div className={path === 'singIn' ? 'active' : ''}>
-  //         <SingIn />
-  //         <S.SingInCard>
-  //           <h2>Não é cadastrado?</h2>
-  //           <p>Cadastre-se! É simples e rápido!</p>
-
-  //           <ButtonOutline onClick={() => setPath('singUp')}>
-  //             Cadastre-se
-  //           </ButtonOutline>
-  //         </S.SingInCard>
-  //       </div>
-  //     </S.SingContainer>
-  //   </S.Container>
-  // )
 }
 
 export default Login
