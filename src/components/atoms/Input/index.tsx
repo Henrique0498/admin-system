8a161ffff0035067bfe26cb4a7495b3be9639b7e
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-09-16 18:05:59 -0300

    Change - Input

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,7 +1,7 @@
 import { InputProps } from './types'
 
 import * as S from './styles'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { IconButton, InputAdornment } from '@mui/material'
 import { MdOutlineVisibility, MdOutlineVisibilityOff } from 'react-icons/md'
 
@@ -16,6 +16,8 @@ const Input = ({
   onChange
 }: InputProps) => {
   const [typeModified, setTypeModified] = useState(type)
+
+  const ref = useRef<HTMLInputElement>(null)
 
   function handleVisible() {
     setTypeModified(typeModified === 'text' ? 'password' : 'text')
@@ -44,20 +46,38 @@ const Input = ({
     return null
   }
 
+  // return (
+  //   <S.Container
+  //     onBlur={validate}
+  //     type={typeModified}
+  //     variant={variant}
+  //     label={label}
+  //     color={color}
+  //     value={value}
+  //     onChange={onChange}
+  //     error={error ? true : false}
+  //     InputProps={{
+  //       endAdornment: renderEndAdornment()
+  //     }}
+  //   />
+  // )
+
+  console.log(ref.current?.value)
+
   return (
-    <S.Container
-      onBlur={validate}
-      type={typeModified}
-      variant={variant}
-      label={label}
-      color={color}
-      value={value}
-      onChange={onChange}
-      error={error ? true : false}
-      InputProps={{
-        endAdornment: renderEndAdornment()
-      }}
-    />
+    <S.Container>
+      <input
+        type="text"
+        name="teste"
+        id="TESTE"
+        ref={ref}
+        placeholder=" "
+        className="input_selector"
+      />
+      <fieldset>
+        <legend>teste teste</legend>
+      </fieldset>
+    </S.Container>
   )
 }
 
--- src/components/atoms/Input/styles.ts
+++ src/components/atoms/Input/styles.ts
@@ -17,52 +17,130 @@ function getColor(error?: boolean, color: ColorsType = 'purple') {
   return theme.colors[color][600]
 }
 
-export const Container = styled(TextField)<InputStylesProps>`
-  ${({ error, color }) => css`
-    & label {
-      &.Mui-focused {
-        color: ${getColor(error, color)};
+// export const Container = styled(TextField)<InputStylesProps>`
+//   ${({ error, color }) => css`
+//     & label {
+//       &.Mui-focused {
+//         color: ${getColor(error, color)};
+//       }
+//     }
+
+//     & .MuiOutlinedInput-root {
+//       & fieldset {
+//         border-color: ${getColor(error, 'gray')};
+//       }
+
+//       &:hover fieldset {
+//         border-color: ${getColor(error, color)};
+//       }
+
+//       &.Mui-focused fieldset {
+//         border-color: ${getColor(error, color)};
+//       }
+//     }
+//   `}
+
+//   button.iconInput[type="button"] {
+//     color: ${({ theme }) => theme.colors.violet[600]};
+
+//     :hover {
+//       background-color: ${({ theme }) =>
+//         transparentize(0.95, theme.colors.violet[600])};
+//     }
+
+//     > span * {
+//       background: ${({ theme }) => theme.colors.violet[400]};
+//     }
+
+//     &.error {
+//       color: ${({ theme }) => theme.colors.red[500]};
+
+//       :hover {
+//         background-color: ${({ theme }) =>
+//           transparentize(0.95, theme.colors.red[500])};
+//       }
+
+//       > span * {
+//         background: ${({ theme }) => theme.colors.red[500]};
+//       }
+//     }
+//   }
+// `
+
+export const Container = styled.div`
+  height: 60px;
+  position: relative;
+  width: 100%;
+  overflow: hidden;
+
+  fieldset {
+    border: 1px solid black;
+    height: 60px;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    border-radius: 4px;
+    z-index: -1;
+
+    legend {
+      transition: 200ms ease-in-out;
+
+      transform: translateY(23px) translateX(12px);
+      font-size: 1rem;
+      width: 0px;
+      height: 22px;
+      padding: 0;
+      white-space: nowrap;
+    }
+  }
+  /*
+  fieldset {
+    border: 1px solid black;
+    height: 60px;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    border-radius: 4px;
+    z-index: -1;
+
+    legend {
+      transition: 200ms ease-in-out;
+      width: 0px;
+      height: 22px;
+      white-space: nowrap;
+    }
+  }
+
+  .input_selector:placeholder-shown ~ fieldset {
+    background: rgb(0, 0, 0, 0.2);
+
+    legend {
+      transform: translateY(23px) translateX(12px);
+      font-size: 1rem;
+      padding: 0;
+    }
+  } */
+
+  &:focus-within {
+    fieldset {
+      border: 2px solid black;
+      legend {
+        transform: none;
+        font-size: 0.875rem;
+        margin-left: 5px;
+        padding: 0 4px;
+        width: fit-content;
       }
     }
+  }
 
-    & .MuiOutlinedInput-root {
-      & fieldset {
-        border-color: ${getColor(error, 'gray')};
-      }
-
-      &:hover fieldset {
-        border-color: ${getColor(error, color)};
-      }
-
-      &.Mui-focused fieldset {
-        border-color: ${getColor(error, color)};
-      }
-    }
-  `}
-
-  button.iconInput[type="button"] {
-    color: ${({ theme }) => theme.colors.violet[600]};
-
-    :hover {
-      background-color: ${({ theme }) =>
-        transparentize(0.95, theme.colors.violet[600])};
-    }
-
-    > span * {
-      background: ${({ theme }) => theme.colors.violet[400]};
-    }
-
-    &.error {
-      color: ${({ theme }) => theme.colors.red[500]};
-
-      :hover {
-        background-color: ${({ theme }) =>
-          transparentize(0.95, theme.colors.red[500])};
-      }
-
-      > span * {
-        background: ${({ theme }) => theme.colors.red[500]};
-      }
-    }
+  input {
+    height: 100%;
+    border-radius: 4px;
+    background-color: transparent;
+    margin-top: 11px;
+    height: calc(100% - 10px);
+    width: 100%;
+    padding: 0 12px;
   }
 `
