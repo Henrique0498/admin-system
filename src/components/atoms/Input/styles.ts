9a77121d757f55e88090d032bd715f872ba31786
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2022-09-25 16:46:52 -0300

    Update - Input

--- src/components/atoms/Input/styles.ts
+++ src/components/atoms/Input/styles.ts
@@ -20,7 +20,7 @@ export const Container = styled.div<InputStylesProps>`
     }
   `}
 
-  height: 54px;
+  height: 3.375rem;
   overflow: hidden;
   position: relative;
   width: 100%;
@@ -28,10 +28,11 @@ export const Container = styled.div<InputStylesProps>`
 
   fieldset {
     color: ${theme.colors.gray[500]};
-    border: 1px solid currentColor;
-    border-radius: 4px;
+    border: 0.063rem solid currentColor;
+    box-sizing: border-box;
+    border-radius: 0.25rem;
     bottom: 0;
-    height: 54px;
+    height: 3.375rem;
     position: absolute;
     width: 100%;
     z-index: -1;
@@ -39,25 +40,27 @@ export const Container = styled.div<InputStylesProps>`
 
     legend {
       transition: 200ms ease-in-out;
-      transform: translateY(20px) translateX(12px);
+      transform: translateY(1.188rem) translateX(0.75rem);
       font-size: 1rem;
-      width: 0px;
-      height: 21px;
-      padding: 0;
+      font-weight: 400;
+      width: 0rem;
+      height: 1.313rem;
+      padding: 0rem;
       white-space: nowrap;
     }
   }
 
   input {
     background-color: transparent;
-    border-radius: 4px;
+    border-radius: 0.25rem;
     color: currentColor;
-    height: calc(100% - 14px);
+    height: calc(100% - 0.875rem);
     flex: 1;
+    font-weight: 400;
     margin-top: 0.75rem;
     margin-left: 0.125rem;
     margin-right: 0.125rem;
-    padding: 0 12px;
+    padding: 0rem 0.75rem;
 
     &:-webkit-autofill,
     &:-webkit-autofill:hover,
@@ -78,27 +81,27 @@ export const Container = styled.div<InputStylesProps>`
     legend {
       font-size: 0.875rem;
       line-height: 1.313rem;
-      margin-left: 5px;
+      margin-left: 0.5rem;
       position: 0;
-      padding: 0 4px;
+      padding: 0rem 0.25rem;
       transform: none;
       width: fit-content;
     }
   }
 
   &:focus-within fieldset {
-    border: 2px solid currentColor;
+    border: 0.125rem solid currentColor;
   }
 `
 
 export const InputButtonsAdornment = styled.div`
-  margin-top: 10px;
-  height: calc(100% - 10px);
+  margin-top: 0.625rem;
+  height: calc(100% - 0.625rem);
   display: flex;
   align-items: center;
 
   > .input_button {
-    margin: 0 0.25rem;
+    margin: 0rem 0.25rem;
     padding: 0.375rem;
     border-radius: 50%;
 
