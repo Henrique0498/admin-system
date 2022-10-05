dc9719141992d7efd4307501839782f4adbb135a
Henrique Lopes <h.lopes.silva2015@gmail.com>
2022-10-05 14:42:59 -0300

    Update - Checkbox

--- src/components/atoms/Checkbox/index.tsx
+++ src/components/atoms/Checkbox/index.tsx
@@ -1,16 +1,26 @@
+import { CheckIcon } from '@heroicons/react/24/outline'
+import { Typography } from '../Typography'
 import * as S from './styles'
 import { CheckboxProps } from './types'
 
 export function Checkbox({ children }: CheckboxProps) {
   return (
     <S.Container>
-      <input
-        type="checkbox"
-        name="teste"
-        id="teste"
-        onChange={(e) => console.log(e)}
-      />
-      {children}
+      <S.ContainerCheckbox>
+        <input
+          type="checkbox"
+          name="teste"
+          className="checkbox"
+          id="teste"
+          onChange={(e) => console.log(e.target.checked)}
+        />
+
+        <S.CustomCheckbox className="custom-checkbox">
+          <CheckIcon className="icon_check" />
+        </S.CustomCheckbox>
+      </S.ContainerCheckbox>
+
+      <Typography className="body">{children}</Typography>
     </S.Container>
   )
 }
--- src/components/atoms/Checkbox/styles.ts
+++ src/components/atoms/Checkbox/styles.ts
@@ -3,4 +3,80 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   align-items: center;
+  gap: 0.25rem;
+
+  .body {
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+  }
+`
+
+export const ContainerCheckbox = styled.div`
+  width: 1.5rem;
+  height: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .checkbox {
+    opacity: 0;
+    position: absolute;
+    width: 1rem;
+    height: 1rem;
+    cursor: pointer;
+  }
+
+  .checkbox:checked ~ .custom-checkbox {
+    background: ${({ theme }) => theme.colors.purple[500]};
+    color: ${({ theme }) => theme.colors.purple[600]};
+  }
+
+  .checkbox:hover ~ .custom-checkbox {
+    color: ${({ theme }) => theme.colors.purple[500]};
+
+    ::before {
+      width: 1.5rem;
+      height: 1.5rem;
+    }
+  }
+
+  .checkbox:checked ~ .custom-checkbox {
+    .icon_check {
+      color: ${({ theme }) => theme.colors.purple[100]};
+      visibility: visible;
+      width: 1rem;
+    }
+  }
+`
+
+export const CustomCheckbox = styled.div`
+  width: 1rem;
+  height: 1rem;
+  border-radius: 0.25rem;
+  color: ${({ theme }) => theme.colors.gray[500]};
+  border: 1px solid currentColor;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all ${({ theme }) => theme.animation.duration.medium} linear;
+  cursor: pointer;
+
+  ::before {
+    content: '';
+    width: 0px;
+    height: 0px;
+    background: ${({ theme }) => theme.colors.purple[200]};
+    position: absolute;
+    z-index: -1;
+    transition: all ${({ theme }) => theme.animation.duration.xFast} linear;
+    border-radius: 50%;
+  }
+
+  .icon_check {
+    visibility: hidden;
+    transition: all ${({ theme }) => theme.animation.duration.medium} linear;
+    width: 0rem;
+  }
 `
